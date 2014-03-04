f8e9193843fceba911faeb181930acbe55107d11
Takahiro Ishikawa <takahish0306@yahoo.co.jp>
2014-03-04 13:14:03 +0900

    Added quicklisp, sbcl Fixed bin/init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -8,14 +8,51 @@ DOTFILES=$1
 
 # Puts symbolic links.
 ## emacs
-ln -s $DOTFILES/emacs ~/.emacs.d
+if [ -e ~/.emacs.d ]; then
+    ### -d, -F, --directory allow the superuser to attempt to hard link directories.
+    echo "replace ~/.emacs.d?"
+    read answer
+    case $answer in
+        y)
+            rm -r ~/.emacs.d
+            ln -s $DOTFILES/emacs/ ~/.emacs.d
+            ;;
+        n)
+            ;;
+        *)
+            ;;
+    esac
+else
+    ln -s $DOTFILES/emacs/ ~/.emacs.d
+fi
 
 ## zsh
-ln -s $DOTFILES/zsh/zshrc ~/.zshrc
+ln -si $DOTFILES/zsh/zshrc ~/.zshrc
 
 ## git
-ln -s $DOTFILES/git/gitconfig ~/.gitconfig
-ln -s $DOTFILES/git/gitexclude ~/.gitexclude
+ln -si $DOTFILES/git/gitconfig ~/.gitconfig
+ln -si $DOTFILES/git/gitexclude ~/.gitexclude
 
 ## screen
-ln -s $DOTFILES/screen/screenrc ~/.screenrc
+ln -si $DOTFILES/screen/screenrc ~/.screenrc
+
+## quicklisp
+if [ -e ~/.quicklisp ]; then
+    echo "replace ~/.quicklisp?"
+    read answer
+    case $answer in
+        y)
+            rm -r ~/.quicklisp
+            ln -s $DOTFILES/quicklisp/ ~/.quicklisp
+            ;;
+        n)
+            ;;
+        *)
+            ;;
+    esac
+else
+    ln -si $DOTFILES/quicklisp/ ~/.quicklisp
+fi
+
+## sbcl
+ln -si $DOTFILES/sbcl/sbclrc ~/.sbclrc
