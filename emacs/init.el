4397367233ebb83e9467a29695250dbe6c07c545
Takahiro Ishikawa <qwqkx447@yahoo.co.jp>
2014-06-15 16:06:20 +0900

    Fixed emacs/init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,47 +1,46 @@
-;; load-pathを再起的に定義
+;; load-path
 (let ((default-directory "~/.emacs.d/site-lisp"))
   (setq load-path (cons default-directory load-path))
   (normal-top-level-add-subdirs-to-load-path))
 
-;; auto-installの設定
+;; auto-install
 (require 'auto-install)
 (setq auto-install-directory "~/.emacs.d/site-lisp/auto-install/")
-;(auto-install-update-emacswiki-package-name t) ;; 必要に応じて使用する
+(auto-install-update-emacswiki-package-name t)
 (auto-install-compatibility-setup)
 (setq auto-install-save-confirm nil)
 
-;; 基本設定
-(setq inhibit-startup-message t) ;; 初期画面を表示しない
-(menu-bar-mode nil) ;; メニューバーを表示しない
+;; base
+(setq inhibit-startup-message t)
+(menu-bar-mode nil)
 (require 'tool-bar)
-(tool-bar-mode nil) ;; ツールバーを表示しない
-(line-number-mode t) ;; 現在の行番号を表示する
-(column-number-mode t) ;; 現在の列番号を表示する
-(which-function-mode t) ;; 現在の関数を表示する
-(setq-default indent-tabs-mode nil) ;; タブを使わない
+(tool-bar-mode nil)
+(line-number-mode t)
+(column-number-mode t)
+(which-function-mode t)
+(setq-default indent-tabs-mode nil)
 
-;; その他の設定
-(global-set-key "\C-h" 'delete-backward-char) ;; C-hを1文字削除に変更
-(setq frame-title-format (format "%%b - %s:%%f"  (system-name))) ;; フレームタイトルの設定
-(global-font-lock-mode t) ;; 色をつける
+;; other
+(global-set-key "\C-h" 'delete-backward-char)
+(setq frame-title-format (format "%%b - %s:%%f"  (system-name)))
+(global-font-lock-mode t)
 (require 'scroll-bar)
-(set-scroll-bar-mode 'right) ;; スクロールバーを右側に表示する
-(setq history-length 1000) ;; 履歴をたくさん表示する
+(set-scroll-bar-mode 'right)
+(setq history-length 1000)
 (setq undo-limit 100000)
-(setq undo-strong-limit 130000) ;; undoの数を増やす
-(setq transient-mark-mode t) ;; 範囲に色をつける
+(setq undo-strong-limit 130000)
+(setq transient-mark-mode t)
 (setq scroll-conservatively 1)
-(setq scroll-step 1) ;; スクロールの設定
+(setq scroll-step 1)
 (setq default-frame-alist '((width . 100)
                             (height . 35)
                             (line-spacing . 2)
                             (font . "fontset-14")))
-;(setq make-backup-files nil) ;; バックアップファイルを作らない
-(setq auto-save-default nil) ;; 自動保存を行わない
-(define-key global-map [?¥] [?\\]) ;; ¥の代わりにバックスラッシュを入力する
-(setq locale-coding-system 'utf-8) ;; ansi-termでの日本語の設定
-
-;; 日本語
+;(setq make-backup-files nil)
+(setq auto-save-default nil)
+(define-key global-map [?¥] [?\\])
+(setq locale-coding-system 'utf-8)
+;;; japanese
 ; (prin1 (font-family-list))
 (set-language-environment "UTF-8")
 (set-fontset-font "fontset-default"
@@ -49,18 +48,17 @@
                   (font-spec :family "Hiragino Kaku Gothic ProN"))
 (setq face-font-rescale-alist
       '((".*Hiragino_Kaku_Gothic_ProN.*" . 1.2)))
-
-;; 色（theme）の設定
+;;; theme
 (load-theme 'misterioso t)
 
-;; anythingの設定
+;; anything
 (require 'anything-startup)
 (require 'anything-match-plugin)
-(setq anything-quick-update t) ;; 候補が多い時に体感速度を早くする
-(setq anything-enable-shortcuts 'alphabet) ;; 候補選択ショートカット
-;; anythingのprefixキーを変更する（デフォルトF5 a）
+(setq anything-quick-update t)
+(setq anything-enable-shortcuts 'alphabet)
+;;; changed anything prefix key（default F5 a）
 (custom-set-variables '(anything-command-map-prefix-key " a"))
-;; anythingの画面の表示方法を変更する
+;;; changed anything display
 (define-key anything-map (kbd "M-n") 'anything-next-source)
 (define-key anything-map (kbd "M-p") 'anything-previous-source)
 (defun anything-split-window (buf)
@@ -68,30 +66,29 @@
   (other-window 1)
   (switch-to-buffer buf))
 (setq anything-display-function 'anything-split-window)
-;; 最近使ったファイルの保存数を増やす
+;;; increased resent files
 (when (require 'recent nil t)
   (custum-set-variables '(recentf-save-file "~/.emacs.d/recent"))
   (setq recentf-max-saved-items 3000)
   (recentf-mode 1))
 
-;; popwinの設定
+;; popwin
 (require 'popwin)
 (setq display-buffer-function 'popwin:display-buffer)
 
-;; auto-completeの設定
+;; auto-complete
 (require 'auto-complete)
 (require 'auto-complete-config)
 (global-auto-complete-mode t)
 
-;; SLIMEの設定
-(setq inferior-lisp-program "/usr/local/bin/sbcl") ;; SBCLをCommon Lisp処理系に設定
-;(setq inferior-lisp-program "/usr/local/bin/clisp") ;; CLISPをCommon Lisp処理系に設定
-;(setq inferior-lisp-program "/usr/local/bin/abcl-no-rlwrap") ;; ABCLをCommon Lisp処理系に設定
-;(setq inferior-lisp-program "/usr/local/bin/ccl") ;; CCLをCommon Lisp処理系に設定
+;; slime
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
+;(setq inferior-lisp-program "/usr/local/bin/clisp")
+;(setq inferior-lisp-program "/usr/local/bin/abcl-no-rlwrap")
+;(setq inferior-lisp-program "/usr/local/bin/ccl")
 (require 'slime)
-(slime-setup '(slime-repl slime-fancy slime-banner)) ;; SLIMEのロード
-
-;; popwinのSLIME設定
+(slime-setup '(slime-repl slime-fancy slime-banner))
+;;; popwin on slime
 (push '("*slime-apropos*") popwin:special-display-config)
 (push '("*slime-macroexpansion*") popwin:special-display-config)
 (push '("*slime-description*") popwin:special-display-config)
@@ -100,23 +97,20 @@
 (push '(sldb-mode :stick t) popwin:special-display-config)
 (push '(slime-repl-mode) popwin:special-display-config)
 (push '(slime-connection-list-mode) popwin:special-display-config)
-
-;; ac-slimeのロード
+;;; ac-slime
 (require 'ac-slime)
-;; ac-slimeの設定
 (add-hook 'slime-mode-hook 'set-up-slime-ac)
 (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
 
-;; scala-modeの設定
-(setq scala-interpreter "/usr/local/bin/scala") ;; scalaの処理系を設定
+;; scala-mode
+(setq scala-interpreter "/usr/local/bin/scala")
 (require 'scala-mode-auto)
 (add-hook 'scala-mode-hook
 	  (lambda ()
 	    (scala-electric-mode)))
 
-;; clojure-modeの設定
-;; Emacs 23ではpackage.elを事前時インストールする必要あり
-;; （Emacs 24ではpackage.elは組み込まれている）
+;; clojure-mode
+;;; There needs to install package.el in emacs 23
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
