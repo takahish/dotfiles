ea6f1ecd91762c90963137b34f103fdc9c6d18bf
Takahiro Ishikawa <takahish.nil@gmail.com>
2015-08-12 22:43:54 +0900

    Fixed emacs/init.el, using package.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,94 +1,84 @@
-;; load-path
-(let ((default-directory "~/.emacs.d/site-lisp"))
-  (setq load-path (cons default-directory load-path))
-  (normal-top-level-add-subdirs-to-load-path))
-
-;; auto-install
-(require 'auto-install)
-(setq auto-install-directory "~/.emacs.d/site-lisp/auto-install/")
-;(auto-install-update-emacswiki-package-name t)
-(auto-install-compatibility-setup)
-(setq auto-install-save-confirm nil)
-
-;; package
-(require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
-
 ;; base
-(setq inhibit-startup-message t)
-(menu-bar-mode nil)
 (require 'tool-bar)
+(require 'scroll-bar)
+(menu-bar-mode nil)
 (tool-bar-mode nil)
 (line-number-mode t)
 (column-number-mode t)
 (which-function-mode t)
-(setq-default indent-tabs-mode nil)
-
-;; other
 (global-set-key "\C-h" 'delete-backward-char)
-(setq frame-title-format (format "%%b - %s:%%f"  (system-name)))
 (global-font-lock-mode t)
-(require 'scroll-bar)
 (set-scroll-bar-mode 'right)
-(setq history-length 1000)
-(setq undo-limit 100000)
-(setq undo-strong-limit 130000)
-(setq transient-mark-mode t)
-(setq scroll-conservatively 1)
-(setq scroll-step 1)
-;(setq default-frame-alist '((width . 100)
-;                            (height . 35)
-;                            (line-spacing . 2)
-;                            (font . "fontset-14")))
-;(setq make-backup-files nil)
-(setq auto-save-default nil)
-(define-key global-map [?¥] [?\\])
-(setq locale-coding-system 'utf-8)
-;;; japanese
-; (prin1 (font-family-list))
+(setq-default indent-tabs-mode nil)
 (set-language-environment "UTF-8")
-(set-fontset-font "fontset-default"
-                  'japanese-jisx0208
-                  (font-spec :family "Hiragino Kaku Gothic ProN"))
-(setq face-font-rescale-alist
-      '((".*Hiragino_Kaku_Gothic_ProN.*" . 1.2)))
-;;; theme
-(load-theme 'misterioso t)
-
-;; anything
-(require 'anything-startup)
-(require 'anything-match-plugin)
-(setq anything-quick-update t)
-(setq anything-enable-shortcuts 'alphabet)
-;;; changed anything prefix key（default F5 a）
+(define-key global-map [?¥] [?\\])
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(anything-command-map-prefix-key " a")
- '(safe-local-variable-values (quote ((Syntax . Common-Lisp)))))
-;;; changed anything display
-(define-key anything-map (kbd "M-n") 'anything-next-source)
-(define-key anything-map (kbd "M-p") 'anything-previous-source)
-(defun anything-split-window (buf)
+ '(locale-coding-system 'utf-8)
+ '(frame-title-format (format "%%b - %s:%%f"  (system-name)))
+ '(inhibit-startup-message t)
+ '(history-length 1000)
+ '(undo-limit 100000)
+ '(undo-strong-limit 130000)
+ '(transient-mark-mode t)
+ '(scroll-conservatively 1)
+ '(scroll-step 1)
+ '(auto-save-default nil)
+ '(make-backup-files nil))
+
+;; increased resent files
+(require 'recentf nil t)
+(custom-set-variables
+ '(recentf-save-file "~/.emacs.d/recentf")
+ '(recentf-max-saved-items 3000))
+(recentf-mode t)
+
+;; theme
+(load-theme 'deeper-blue t)
+
+;; package
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
+
+;; required-packages
+(defvar required-packages
+  '(helm
+    popwin
+    auto-complete
+    sr-speedbar
+    slime
+    ac-slime
+    helm-gtags))
+
+(dolist (package required-packages)
+  (unless (package-installed-p package)
+    (package-refresh-contents)
+    (package-install package)))
+
+;; helm
+(require 'helm-config)
+(defun helm-split-window (buf)
   (split-window)
   (other-window 1)
   (switch-to-buffer buf))
-(setq anything-display-function 'anything-split-window)
-;;; increased resent files
-(when (require 'recent nil t)
-  (custum-set-variables '(recentf-save-file "~/.emacs.d/recent"))
-  (setq recentf-max-saved-items 3000)
-  (recentf-mode 1))
+;;; changed prefix key etc
+(custom-set-variables
+ '(helm-quick-update t)
+ '(helm-enable-shortcuts 'alphabet)
+ '(helm-command-prefix-key " a")
+ '(safe-local-variable-values (quote ((Syntax . Common-Lisp))))
+ '(helm-display-function 'helm-split-window))
+;;; changed helm display
+(define-key helm-command-map (kbd "M-n") 'helm-next-source)
+(define-key helm-command-map (kbd "M-p") 'helm-previous-source)
 
 ;; popwin
 (require 'popwin)
-(setq display-buffer-function 'popwin:display-buffer)
+(custom-set-variables
+ '(display-buffer-function 'popwin:display-buffer))
 
 ;; auto-complete
 (require 'auto-complete)
@@ -96,12 +86,14 @@
 (global-auto-complete-mode t)
 
 ;; org
-(setq org-log-done 'time)
+(custom-set-variables
+ '(org-log-done 'time))
 
 ;; sr-speedbar
 (require 'sr-speedbar)
-(setq sr-speedbar-right-side nil)
-(setq speedbar-use-images nil)
+(custom-set-variables
+ '(sr-speedbar-right-side nil)
+ '(speedbar-use-images nil))
 (sr-speedbar-refresh-turn-off)
 (global-set-key (kbd "C-^") 'sr-speedbar-toggle)
 (add-hook 'speedbar-mode-hook
@@ -109,22 +101,28 @@
              (speedbar-add-supported-extension
               '("lisp"
                 "c"
+                "cpp"
+                "java"
                 "py"
                 "rb"
                 "pl"
                 "scala"
                 "clj"
-                "org"
+                "go"
+                "pig"
                 "html"
                 "xml"
-                "*"))))
+                "org"
+                "txt"))))
 (provide 'init_speedbar)
 
 ;; slime
-(setq inferior-lisp-program "/usr/local/bin/sbcl")
-;(setq inferior-lisp-program "/usr/local/bin/clisp")
-;(setq inferior-lisp-program "/usr/local/bin/abcl-no-rlwrap")
-;(setq inferior-lisp-program "/usr/local/bin/ccl")
+(setq-default
+ inferior-lisp-program "/usr/local/bin/sbcl")
+; inferior-lisp-program "/usr/local/bin/clisp")
+; inferior-lisp-program "/usr/local/bin/abcl-no-rlwrap")
+; inferior-lisp-program "/usr/local/bin/ccl")
+
 (require 'slime)
 (slime-setup '(slime-repl slime-fancy slime-banner))
 ;;; popwin on slime
@@ -141,32 +139,11 @@
 (add-hook 'slime-mode-hook 'set-up-slime-ac)
 (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
 
-;; global
-;;; not use shipped gtags.el
-;;; M-x auto-install-from-url https://raw.github.com/voins/gtags/master/gtags.el
-;;; M-x auto-install-from-emacs-wiki anything-gtags
-(autoload 'gtags-mode "gtags" "" t)
-(setq gtags-mode-hook
+;; helm-gtags
+(autoload 'helm-gtags-mode "helm-gtags" "" t)
+(add-hook 'helm-gtags-mode-hook
       '(lambda ()
-         (local-set-key "\M-t" 'gtags-find-tag)
-         (local-set-key "\M-r" 'gtags-find-rtag)
-         (local-set-key "\M-s" 'gtags-find-symbol)
-         (local-set-key "\C-t" 'gtags-pop-stack)))
-
-;; scala-mode
-(unless (package-installed-p 'ensime)
-  (package-refresh-contents)
-  (package-install 'ensime))
-(require 'ensime)
-(push "/usr/local/bin" exec-path)
-(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
-
-;; clojure-mode
-;;; There needs to install package.el in emacs 23
-(unless (package-installed-p 'clojure-mode)
-  (package-refresh-contents)
-  (package-install 'clojure-mode))
-(add-hook 'clojure-mode-hook 'paredit-mode)
-
-;; pig-mode
-(require 'pig-mode)
+         (local-set-key "\M-t" 'helm-gtags-find-tag)
+         (local-set-key "\M-r" 'helm-gtags-find-rtag)
+         (local-set-key "\M-s" 'helm-gtags-find-symbol)
+         (local-set-key "\C-t" 'helm-gtags-pop-stack)))
