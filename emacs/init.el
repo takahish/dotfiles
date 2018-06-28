3090edde424f428224ce056ec4319a8635d252ec
Takahiro Ishikawa <takahish.nil@gmail.com>
2018-06-28 23:27:10 +0900

    Fixed init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,78 +1,43 @@
-;; base
-(require 'tool-bar)
-(require 'scroll-bar)
-(menu-bar-mode nil)
-(tool-bar-mode nil)
-(line-number-mode t)
-(column-number-mode t)
-(which-function-mode t)
-(global-set-key "\C-h" 'delete-backward-char)
-(global-font-lock-mode t)
-(set-scroll-bar-mode 'right)
-(setq-default indent-tabs-mode nil)
-(set-language-environment "UTF-8")
-(define-key global-map [?¥] [?\\])
-(set-default-font "Inconsolata-14")
+;;; Global setting.
+;;; ==========================================================================
+(global-set-key "\C-h" 'delete-backward-char) ; bind \C-h to deleting char.
+(global-font-lock-mode t)                     ; Font look is True.
+(setq-default indent-tabs-mode nil)           ; Indent is space only.
+(set-language-environment "UTF-8")            ; Charactor code is UTF-8.
+(setq-default inhibit-splash-screen t)        ; Remove splash screen.
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ac-use-fuzzy t)
- '(ac-use-menu-map t)
- '(auto-save-default nil)
- '(display-buffer-function (quote popwin:display-buffer))
- '(frame-title-format (format "%%b - %s:%%f" (system-name)) t)
- '(helm-command-prefix-key " a")
- '(helm-display-function (quote helm-split-window))
- '(helm-enable-shortcuts (quote alphabet))
- '(helm-quick-update t)
- '(history-length 1000)
- '(inhibit-startup-screen t)
- '(locale-coding-system (quote utf-8) t)
- '(make-backup-files nil)
- '(org-log-done (quote time))
- '(package-selected-packages
-   (quote
-    (yaml-mode sr-speedbar popwin helm-gtags go-mode go-autocomplete ac-slime)))
- '(recentf-max-saved-items 3000)
- '(recentf-save-file "~/.emacs.d/recentf")
- '(safe-local-variable-values (quote ((Syntax . Common-Lisp))))
- '(scroll-conservatively 1)
- '(scroll-step 1)
- '(transient-mark-mode t)
- '(undo-limit 100000)
- '(undo-strong-limit 130000))
+;; Theme setting.
+(load-theme 'wombat t)
 
-;; increased resent files
-(require 'recentf nil t)
-(recentf-mode t)
+;; Common lisp setting.
+(setq-default inferior-lisp-program "/usr/local/bin/sbcl")           ; SBCL
+;(setq-default inferior-lisp-program "/usr/local/bin/clisp")          ; CLisp
+;(setq-default inferior-lisp-program "/usr/local/bin/abcl-no-rlwrap") ; ABCL
+;(setq-default inferior-lisp-program "/usr/local/bin/ccl")            ; ClozureCL
 
-;; theme
-(load-theme 'misterioso t)
 
-;; load-path
-(add-to-list 'load-path "~/bin")
-
-;; package
+;;; Package setting.
+;;; ==========================================================================
 (require 'package)
+
+;; Added package-archives.
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
+;; Initialization.
 (package-initialize)
 
-;; required-packages
+;; Required packages.
 (defvar required-packages
-  '(helm
-    popwin
+  '(popwin
+    helm
     auto-complete
     slime
-    ac-slime
-    helm-gtags))
+    ac-slime))
 
-;; no-installed-packages
+;; Defun helper function.
 (defun check-installed (pkgs)
   (if (null pkgs)
       nil
@@ -80,6 +45,7 @@
         (cons (car pkgs) (check-installed (cdr pkgs)))
       (check-installed (cdr pkgs)))))
 
+;; Install not installed package.
 (let ((not-installed-packages
        (check-installed required-packages)))
   (when not-installed-packages
@@ -87,70 +53,114 @@
     (dolist (package not-installed-packages)
       (package-install package))))
 
-;; delete-trailing-whitespace
+
+;;; Recentf setting.
+;;; ==========================================================================
+(require 'recentf)
+(recentf-mode t)
+
+;; Set custom variables.
+(custom-set-variables
+ '(recentf-save-file "~/.emacs.d/recentf")
+ '(recentf-max-saved-items 3000) ; increased resent files
+ '(recentf-auto-save-timer (run-with-idle-timer 30 t 'recentf-save-list)))
+
+
+;;; Whitespace setting.
+;;; ==========================================================================
+(require 'whitespace)
+
+;; Set custom variables.
+(custom-set-variables
+ '(whitespace-style
+   '(face trailing tabs spaces empty space-mark tab-mark))
+ '(whitespace-display-mappings
+   '((space-mark ?\u3000 [?\u25a1])
+     (tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
+ '(whitespace-space-regexp "\\(\u3000+\\)"))
+
+;; Whitespace is True.
+(global-whitespace-mode t)
+
+;; Hook saving file, delete whitespace before close.
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-;; helm
-(require 'helm-config)
-(defun helm-split-window (buf)
-  (split-window)
-  (other-window 1)
-  (switch-to-buffer buf))
-;;; changed prefix key etc
 
-;;; changed helm display
-(define-key helm-command-map (kbd "M-n") 'helm-next-source)
-(define-key helm-command-map (kbd "M-p") 'helm-previous-source)
-
-;; popwin
+;;; Popwin setting.
+;;; ==========================================================================
 (require 'popwin)
 
+;; Set custom variables.
+(custom-set-variables
+  '(display-buffer-function 'popwin:display-buffer)
+  '(popwin:popup-window-position 'bottom)
+  '(popwin:special-display-config '(("*complitation*" :noselect t))))
 
-;; auto-complete
+
+;;; Helm setting.
+;;; ==========================================================================
+(require 'helm)
+
+;; Basic configulation.
+(require 'helm-config)
+
+;; Set custom variables.
+(custom-set-variables
+  '(helm-command-prefix-key "^X a")
+  '(helm-display-function #'display-buffer) ; For using popwin:display-buffer.
+  '(helm-enable-shortcuts 'alphabet)
+  '(helm-quick-update t)
+  '(popwin:special-display-config
+    (cons '("helm" :regexp t :height 0.4) popwin:special-display-config)))
+
+
+;;; Auto-complete setting.
+;;; ==========================================================================
+(require 'auto-complete)
 (require 'auto-complete-config)
+
+;; Basic configulation.
 (ac-config-default)
-(global-auto-complete-mode t)
+
+;; Set custom variables.
+(custom-set-variables
+ '(global-auto-complete-mode t)
+ '(ac-use-fuzzy t)
+ '(ac-use-menu-map t)
+ '(ac-auto-start t))
+
+;; Set key.
+(define-key ac-complete-mode-map "\C-n" 'ac-next)
+(define-key ac-complete-mode-map "\C-p" 'ac-previous)
+
+;; Add mode.
+(add-to-list 'ac-modes 'fundamental-mode)
+(add-to-list 'ac-modes 'sql-mode)
 
 
-;; org
-
-
-;; common-lisp
-(setq-default
- inferior-lisp-program "/usr/local/bin/sbcl")
-; inferior-lisp-program "/usr/local/bin/clisp")
-; inferior-lisp-program "/usr/local/bin/abcl-no-rlwrap")
-; inferior-lisp-program "/usr/local/bin/ccl")
-
-;; slime
+;;; Slime setting.
+;;; ==========================================================================
+;; Load slime setting when it is used.
 (autoload 'slime "slime" nil t)
+
+;; Initialization.
 (with-eval-after-load 'slime
   (slime-setup '(slime-repl slime-fancy slime-banner))
-  ;;; popwin on slime
-  (push '("*slime-apropos*") popwin:special-display-config)
-  (push '("*slime-macroexpansion*") popwin:special-display-config)
-  (push '("*slime-description*") popwin:special-display-config)
-  (push '("*slime-compilation*" :noselect t) popwin:special-display-config)
-  (push '("*slime-xref*") popwin:special-display-config)
-  (push '(sldb-mode :stick t) popwin:special-display-config)
-  (push '(slime-repl-mode) popwin:special-display-config)
-  (push '(slime-connection-list-mode) popwin:special-display-config)
-  ;;; ac-slime
-  (require 'ac-slime))
+  ;; Set custom variables.
+  (custom-set-variables
+   '(popwin:special-display-config
+     (append '(("*slime-apropos*")
+               ("*slime-macroexpansion*")
+               ("*slime-description*")
+               ("*slime-compilation*" :noselect t)
+               ("*slime-xref*")
+               (sldb-mode :stick t)
+               (slime-repl-mode)
+               (slime-connection-list-mode))
+             popwin:special-display-config)
+     ;; auto-complete
+     (require 'ac-slime))))
+
+;; Hook lunching slime.
 (add-hook 'slime-mode-hook 'set-up-slime-ac)
 (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
-
-;; helm-gtags
-(autoload 'helm-gtags-mode "helm-gtags" nil t)
-(add-hook 'helm-gtags-mode-hook
-          '(lambda ()
-             (local-set-key "\M-t" 'helm-gtags-find-tag)
-             (local-set-key "\M-r" 'helm-gtags-find-rtag)
-             (local-set-key "\M-s" 'helm-gtags-find-symbol)
-             (local-set-key "\C-t" 'helm-gtags-pop-stack)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
